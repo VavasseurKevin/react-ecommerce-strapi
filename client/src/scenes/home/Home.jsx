1ef73786cdbebae889e7adb96a77a5f80999f095
VavasseurKevin <minhluan.phan@gmail.com>
2024-04-09 21:47:10 +0200

    Create Subscribe.jsx in Home

--- client/src/scenes/home/Home.jsx
+++ client/src/scenes/home/Home.jsx
@@ -1,12 +1,14 @@
 import { Carousel } from "react-responsive-carousel";
 import ShoppingList from "./ShoppingList";
 import MainCarousel from "./MainCarousel";
+import Subscribe from "./Subscribe";
 
 const Home = () => {
     return (
         <div>
             <MainCarousel/>
             <ShoppingList/>
+            <Subscribe/>
         </div>
     )
 }
