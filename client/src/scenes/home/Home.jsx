1193921ea9a857ef4f61d8dc61a20298068536db
VavasseurKevin <minhluan.phan@gmail.com>
2024-04-09 19:37:13 +0200

    Create MainCarousel.jsx in Home

--- client/src/scenes/home/Home.jsx
+++ client/src/scenes/home/Home.jsx
@@ -1,8 +1,11 @@
+import { Carousel } from "react-responsive-carousel";
 import ShoppingList from "./ShoppingList";
+import MainCarousel from "./MainCarousel";
 
 const Home = () => {
     return (
         <div>
+            <MainCarousel/>
             <ShoppingList/>
         </div>
     )
