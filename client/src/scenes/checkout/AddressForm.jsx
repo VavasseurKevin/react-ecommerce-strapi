b6dd830ac4b2b2001427f060d518aa05a1f8c57e
VavasseurKevin <minhluan.phan@gmail.com>
2024-04-16 16:41:31 +0200

    WIP : AdresseForm

--- client/src/scenes/checkout/AddressForm.jsx
+++ client/src/scenes/checkout/AddressForm.jsx
@@ -1,10 +1,60 @@
+import { getIn } from "formik";
+import { Box } from "@mui/material";
+import TextField from "@mui/material/TextField";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
-const AddressForm = ({}) => {
-    return(
-        <Box>
-            
-        </Box>
-    )
+const AddressForm = ({
+  type,
+  values,
+  touched,
+  errors,
+  handleBlur,
+  handleChange,
+}) => {
+  const isNonMobile = useMediaQuery("(min-width:600px)");
 
-}
+  // dynamiquement des noms de champs en ajoutant un préfixe spécifique
+  const formattedName = (field) => `${type}.${field}`;
+
+  // vérifier si un champ spécifique a été touché et s'il a une erreur associée dans un formulaire.
+  const formattedError = (field) =>
+    Boolean(
+      getIn(touched, formattedName(field)) &&
+        getIn(errors, formattedName(field))
+    );
+
+    const formattedHelper = (field) =>
+    getIn(touched, formattedName(field)) && getIn(errors, formattedName(field));
+
+  return (
+    <Box
+      display="grid"
+      gap="15px"
+      gridTemplateColumns="repeat(4, minmax(0, 1fr))"
+      sx={{
+        "& > div": { gridColumn: isNonMobile ? undefined : "span 4" },
+      }}
+    >
+      <TextField
+        fullWidth
+        type="text"
+        label="First Name"
+        onBlur={handleBlur}
+        onChange={handleChange}
+        value={values.firstName}
+        name={formattedName("firstName")}
+        error={formattedName("firstName")}
+        helperText={formattedHelper("firstName")}
+        sx = {{gridColumn: "span 2"}}
+      />
+      <TextField />
+      <TextField />
+      <TextField />
+      <TextField />
+      <TextField />
+      <TextField />
+      <TextField />
+    </Box>
+  );
+};
 export default AddressForm;
