06ad74ffd8abfa66b96a43a60a5dcfd336e11343
VavasseurKevin <minhluan.phan@gmail.com>
2024-04-25 21:08:11 +0200

    create a stripe session + create the item + return the session id in orders.jsx

--- server/src/api/order/controllers/order.js
+++ server/src/api/order/controllers/order.js
@@ -1,5 +1,6 @@
 "use strict";
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
 /**
  * order controller
  */
@@ -8,7 +9,7 @@ const { createCoreController } = require("@strapi/strapi").factories;
 
 module.exports = createCoreController("api::order.order", ({ strapi }) => ({
   async create(ctx) {
-    const { products, username, email } = ctx.request.body;
+    const { products, userName, email } = ctx.request.body;
     try {
       // retrieve item information
       const lineItems = await Promise.all(
@@ -29,7 +30,27 @@ module.exports = createCoreController("api::order.order", ({ strapi }) => ({
           };
         })
       );
-      
-    } catch (error) {}
+
+      // create a stripe session
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ["card"],
+        customer_email: email,
+        mode: "payment",
+        success_url: "http://localhost:3000/checkout/success",
+        cancel_url: "http://localhost:3000",
+        line_items: lineItems,
+      });
+
+      // create the item
+      await strapi
+        .service("api::order.order")
+        .create({ data: { userName, products, stripeSessionId: session.id } });
+
+      // return the session id
+      return { id: session.id };
+    } catch (error) {
+      ctx.response.status = 500;
+      return { error: { message: "There was a problem creating the charge" } };
+    }
   },
 }));
